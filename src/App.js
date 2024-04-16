d5bd548929154750aa7d8601aabe3d40f34dbc6f
WebsiteDeployer <respected_corporate_analyst@protonmail.com>
2024-04-16 14:04:29 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography } from 'antd';
 import './App.css';
+import kehoe from './images/Kehoe Mcconnell Roe B&W.png';
 
 const { Title } = Typography;
 
 function App() {
+  const [photoWidth, setPhotoWidth] = useState(null);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = kehoe; // Use the rectangular image to calculate the width
+    img.onload = () => {
+      const aspectRatio = img.width / img.height;
+      const calculatedWidth = 200 * aspectRatio; // Adjust the multiplier as needed
+      setPhotoWidth(calculatedWidth);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        {isImageLoaded && <div className="blur-background"></div>}
         <div className="photo-section">
-          <img src="KEHOE B&W FINAL 041624.png" alt="Mike Kehoe" />
+          <img src={kehoe} alt="Mike Kehoe" className="photo" style={{ width: 700 }} onLoad={() => setIsImageLoaded(true)} />
         </div>
         <div className="slide-in-text">
-          <Title level={3}>Missouri Lieutenant Governor Mike Kehoe's payments to Axiom Strategies are concerning.</Title>
+          <Title level={3} style={{ fontFamily: 'Roboto', color: '#333', textAlign: 'center', marginTop: '20px', fontSize: '28px', fontWeight: 'bold', textTransform: 'uppercase' }}>
+            Missouri Lieutenant Governor Mike Kehoe's payments to Axiom Strategies are concerning.
+          </Title>
         </div>
         <div className="payment-boxes">
           <div className="payment-box">
